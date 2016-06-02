b230ae20b8017c14d2ef3fd35df08722efdeb4a2
Aditya Agrawal <exploitprotocol@users.noreply.github.com>
2016-06-02 19:18:05 +0530

    suppress error
    This will suppress error which is seen when xvwa is setup on windows. 
    
    Error is "Deprecated: mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead in C:\xampp\htdocs\xvwa\config.php on line 7"

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $host = "localhost";
 $dbname = 'xvwa';
 $user = 'root';
 $pass = '';
-$conn = mysql_connect($host,$user,$pass);
+$conn = @mysql_connect($host,$user,$pass);
 $conn = mysql_select_db($dbname);
 $conn1 = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 $conn1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
