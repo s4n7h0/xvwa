b91e3e543d6a8a3ea32dd61fcd1b78c60068c1f0
s4n7h0 <saminside34@gmail.com>
2017-01-20 23:21:27 +0800

    bug fixes with php 7

--- config.php
+++ config.php
@@ -5,7 +5,6 @@ $dbname = 'xvwa';
 $user = "root";
 $pass = "";
 $conn = new mysqli($host,$user,$pass,$dbname);
-//$conn = mysql_select_db($dbname);
 $conn1 = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 $conn1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
